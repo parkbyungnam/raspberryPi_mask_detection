da7f6b7a868aa29810502070d05c0aa7c6a4f063
Park_ByungNam <tom3k@naver.com>
2020-11-24 16:42:16 +0900

    test

--- tfliteConvert.py
+++ tfliteConvert.py
@@ -1,7 +1,13 @@
+from tensorflow import keras
+
+model = keras.models.load_model('models/mask_detector.model',compile=False)
+export_path = 'models/'
+model.save(export_path,save_format = 'tf')
+
 import tensorflow as tf
 
-coverter = tf.lite.TFLiteConverter.from_saved_model(r'models')
+saved_model_dir = 'models/'
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+converter.target_spec.supported_ops=[tf.lite.OpsSet.TFLITE_BUILTINS,tf.lite.OpsSet.SELECT_TF_OPS]
 tflite_model = converter.convert()
-
-with open('modelTest.tflite','wb') as f:
-    f.write(tflite_model)
+open('models/converted_model.tflite','wb').write(tflite_model)
